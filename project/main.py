693bdb3e962c40214d7bf64531a075b9a4e66e89
Bensuperpc <bensuperpc@gmail.com>
2022-09-07 15:39:02 +0200

    Update share
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- project/main.py
+++ project/main.py
@@ -34,11 +34,6 @@ def mutual():
     return render_template("mutual.html", mutuals=mutuals)
 
 
-@main.route("/share")
-def share():
-    return render_template("share.html")
-
-
 @main.route("/about")
 def about():
     return render_template("about.html")
@@ -49,6 +44,25 @@ def article():
     posts = Post.query.all()
     logger.info(posts)
     return render_template("article.html", posts=posts)
+
+
+@main.route("/share")
+def share():
+    musics_path = os.path.join(main.root_path, "static/media/music/")
+    musics = os.listdir(musics_path)
+    musics.remove(".gitkeep")
+    logger.info(f"musics: {musics}")
+
+    images_path = os.path.join(main.root_path, "static/media/image/")
+    images = os.listdir(images_path)
+    images.remove(".gitkeep")
+    logger.info(f"images: {images}")
+
+    videos_path = os.path.join(main.root_path, "static/media/video/")
+    videos = os.listdir(videos_path)
+    videos.remove(".gitkeep")
+    logger.info(f"videos: {videos}")
+    return render_template("share.html", musics=musics, images=images, videos=videos)
 
 
 @main.route("/media/music/<path:filename>")
